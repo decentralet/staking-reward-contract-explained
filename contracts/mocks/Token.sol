b43f31d2623bf928b0609f52ceb2e7e05e0b278c
Developer Uche <nweleuchenna2004@gmail.com>
2022-12-28 10:01:23 +0100

    developeruche: added mock token contract

---
+++ contracts/mocks/Token.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.5.16;
+
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+
+
+contract MockToken is ERC20 {
+      uint256 constant initialSupply = 100000000 * (10 ** 18);
+      constructor() public ERC20() {
+          _mint(msg.sender, initialSupply);
+      }
+}
