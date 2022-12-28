960d132a4c352802c51293a9bb35810ebf71096b
Developer Uche <nweleuchenna2004@gmail.com>
2022-12-28 01:52:26 +0100

    developeruche: editted hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,16 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
+	solidity: {
+		compilers: [
+			{
+				version: '0.4.25',
+			},
+			{
+				version: '0.5.16',
+			},
+		],
+	},
 };
 
 export default config;
